a4ca34b0efaa377fc61b654ced5ccca8a15022b3
sin528 <satowimp0319@gmail.com>
2020-09-02 14:32:18 +0900

    2020/09/02

---
+++ IrasutoyaGenerator.py
@@ -0,0 +1,24 @@
+import keras as K
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+seed = 502
+
+z_dim = 100
+
+model = K.models.load_model('saved_model/20000_Noda.h5')
+
+np.random.seed(seed=seed)
+z = np.random.normal(0, 1, (1, z_dim))
+
+img = model.predict(z)
+img = np.reshape(img, (64, 64, 3))
+print(img.shape)
+
+img = 0.5 * img + 0.5
+
+plt.imsave('generate_img/irasuto.jpg', img)
+plt.imshow(img)
+plt.show()
